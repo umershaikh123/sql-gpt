df4cd92f4df62093f9dbf732f2639bbf27ed6a77
Muhammadumer.eth <umershaikh217@gmail.com>
2023-07-20 20:02:23 +0500

    check

--- components/NewChatButton.tsx
+++ components/NewChatButton.tsx
@@ -8,6 +8,8 @@ import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutline
 import { promises as fsPromises } from 'fs'
 import anime from 'animejs/lib/anime.es.js'
 import { motion } from 'framer-motion'
+import PocketBase from 'pocketbase'
+
 interface NewChatButtonProps {
   url: string
 }
@@ -18,160 +20,8 @@ interface Chat {
   messages: string[]
 }
 
-// async function getchatHistory () {
-//     const fileData = await fsPromises.readFile('chats.json', 'utf-8');
-// }
-
-// const NewChatButton: React.FC<NewChatButtonProps> = ({ url }) => {
-//   const [ER_SQL_chats, setER_SQL_chats] = useState<Chat[]>([])
-
-//   const [CSV_SQL_chats, setCSV_SQL_Chats] = useState<Chat[]>([])
-
-//   //   const [chats, setChats] = useState<Chat[]>([])
-
-//   const router = useRouter()
-
-//   const generateUniqueChatId = (): string => {
-//     return uuidv4()
-//   }
-
-//   useEffect(() => {
-//     const fetchChats = async () => {
-//       try {
-//         // const response = await fetch('/api/chats');
-//         const response = await fetch('/api/data', {
-//           method: 'GET',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//         })
-//         if (response.ok) {
-//           const data = await response.json()
-//           setER_SQL_chats(data)
-//         } else {
-//           console.error('Failed to fetch chats')
-//         }
-//       } catch (error) {
-//         console.error(error)
-//       }
-//     }
-
-//     fetchChats()
-//   }, [])
-
-//   const handleNewChat = async (): Promise<void> => {
-//     const newChatId = generateUniqueChatId()
-//     const newChat: Chat = {
-//       id: newChatId,
-//       title: `Chat ${newChatId}`,
-//       messages: [],
-//     }
-//     const updatedChats = [...ER_SQL_chats, newChat]
-//     setER_SQL_chats(updatedChats)
-
-//     try {
-//       const response = await fetch('/api/data', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(updatedChats),
-//       })
-
-//       if (!response.ok) {
-//         console.error('Error saving chats')
-//       }
-//     } catch (error) {
-//       console.error(error)
-//     }
-
-//     router.push(`/SubPages/${url}/${newChatId}`)
-//   }
-
-//   const handleRemoveChat = (chatId: string): void => {
-//     const updatedChats = ER_SQL_chats.filter(chat => chat.id !== chatId)
-//     setER_SQL_chats(updatedChats)
-
-//     fetch(`/api/data/${chatId}`, {
-//       method: 'DELETE',
-//     })
-//       .then(response => {
-//         if (!response.ok) {
-//           console.error('Failed to delete chat')
-//         }
-//         if (response.ok) {
-//           console.error('Success')
-//         }
-//       })
-//       .catch(error => {
-//         console.error(error)
-//       })
-//   }
-
-//   return (
-//     <>
-//       <Button
-//         variant="outlined"
-//         color="primary"
-//         startIcon={<AddOutlinedIcon />}
-//         onClick={handleNewChat}
-//         sx={{
-//           fontSize: '14px',
-//           textTransform: 'capitalize',
-//           minWidth: '12rem',
-//         }}
-//       >
-//         New Chat
-//       </Button>
-
-//       {ER_SQL_chats.map(chat => (
-//         <>
-//           <div key={chat.id}>
-//             <ButtonGroup
-//               disableElevation
-//               variant="outlined"
-//               aria-label="Disabled elevation buttons"
-//             >
-//               <Button
-//                 variant="outlined"
-//                 color="primary"
-//                 startIcon={<ChatBubbleOutlineOutlinedIcon />}
-//                 onClick={() => router.push(`/SubPages/${url}/${chat.id}`)}
-//                 sx={{
-//                   textTransform: 'capitalize',
-//                 }}
-//               >
-//                 <Typography
-//                   variant="subtitle2"
-//                   sx={{
-//                     maxWidth: '7rem',
-//                     fontSize: '12px',
-//                     whiteSpace: 'nowrap',
-//                     overflow: 'hidden',
-//                     textOverflow: 'ellipsis',
-//                   }}
-//                 >
-//                   {chat.id}
-//                 </Typography>
-//               </Button>
-
-//               <Button
-//                 variant="outlined"
-//                 color="primary"
-//                 onClick={() => handleRemoveChat(chat.id)}
-//                 sx={{ borderLeft: 'none', width: '0.5rem' }}
-//               >
-//                 <DeleteOutlineOutlinedIcon />
-//               </Button>
-//             </ButtonGroup>
-//           </div>
-//         </>
-//       ))}
-//     </>
-//   )
-// }
-
 const TextER: React.FC<NewChatButtonProps> = ({ url }) => {
+  const pb = new PocketBase('http://127.0.0.1:8090')
   const [TextERchats, setTextERchats] = useState<Chat[]>([])
 
   const router = useRouter()
@@ -340,8 +190,6 @@ const TextER: React.FC<NewChatButtonProps> = ({ url }) => {
 
 const CsvSQL: React.FC<NewChatButtonProps> = ({ url }) => {
   const [CSV_SQL_chats, setCSV_SQL_chats] = useState<Chat[]>([])
-
-  //   const [chats, setChats] = useState<Chat[]>([])
 
   const router = useRouter()
 
